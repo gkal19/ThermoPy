eb4acf210f997f4b7ddcd654a604ee53ee1756df
Gabriel Kalani <gabrielsilva1956@gmail.com>
2017-04-27 15:02:02 -0300

    vai monstro

--- app.py
+++ app.py
@@ -1,36 +1,35 @@
 #!/usr/bin/python
 
-# Unidades
-#---------------
-Celsius = float(input("Escolha um valor em graus Celsius: "))
-Fahrenheit = float(input("Escolha um valor em graus Fahrenheit: "))
-Kelvin = float(input("Escolha um valor em Kelvin: "))
-
 # Variáveis
 #---------------
-FtoC = Fahrenheit - 32  # Fahrenheit para Celsius
-FtoK = (Fahrenheit - 32) / 1.8  # Fahrenheit para Kelvin
 
-KtoF = Kelvin * 1.8  # Kelvin para Fahrenheit
+CELSIUS = float(input("Escolha um valor em graus Celsius: "))
+FAHRENHEIT = float(input("Escolha um valor em graus Fahrenheit: "))
+KELVIN = float(input("Escolha um valor em Kelvin: "))
 
-# Celsius
+FTOC = FAHRENHEIT - 32  # Fahrenheit para Celsius
+FTOK = (FAHRENHEIT - 32) / 1.8  # Fahrenheit para Kelvin
+
+KTOF = KELVIN * 1.8  # Kelvin para Fahrenheit
+
+# Unidades
 #---------------
+
+    # Celsius
+    #---------------
 print("Convertendo seu valor de Celsius para Fahrenheit, o resultado é:\n",
-      Celsius * 1.8 + 32)
+      CELSIUS * 1.8 + 32)
+print("Convertendo seu valor de Celsius para Kelvin, o resultado é:\n", CELSIUS + 273.15)
 
-print("Convertendo seu valor de Celsius para Kelvin, o resultado é:\n", Celsius + 273.15)
-
-# Fahrenheit
-#---------------
+    # Fahrenheit
+    #---------------
 print("Convertendo seu valor de Fahrenheit para Celsius, o resultado é:")
-print(FtoC / 1.8)
-
+print(FTOC / 1.8)
 print("Convertendo seu valor de Fahrenheit para Kelvin, o resultado é:")
-print(FtoK + 273)
+print(FTOK + 273)
 
-# Kelvin
-#---------------
-print("Convertendo seu valor de Kelvin para Celsius, o resultado é:\n", Kelvin - 273.15)
-
+    # Kelvin
+    #---------------
+print("Convertendo seu valor de Kelvin para Celsius, o resultado é:\n", KELVIN - 273.15)
 print("Convertendo seu valor de Kelvin para Fahrenheit, o resultado é:", )
-print(KtoF - 459.67)
+print(KTOF - 459.67)
